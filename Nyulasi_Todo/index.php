8d6bde544ce3c123ff7fe122b8a611ec99484619
raven151 <nyulasi.akos15@gmail.com>
2021-06-09 19:02:16 +0200

    Some changes for adding data
    trying to add data new fields created for that with scripts, have to work on the second script to add data from fields to database

--- Nyulasi_Todo/index.php
+++ Nyulasi_Todo/index.php
@@ -4,6 +4,8 @@
 		<meta charset="UTF-8">
 		<title>Todo-app</title>
 		<link href="res/style.css" rel="stylesheet" type="text/css">
+		<!-- Add icon library -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
 	<body>
 
@@ -62,11 +64,11 @@
                             
                             if($important == 1){
                                 echo'
-                         <td style="float:left;"><input type="checkbox" id="'.$id.'" name="'.$id.'" value="1">'.$task.' <p>Important</p></td>';
+                         <td style="float:left;"><button class="btn" name ="Done" onclick="Done()"><i class="fa fa-circle-o" aria-hidden="true"></i></button>'.$task.' <p>Important</p></td>';
                                 
                             }
                             else{
-                                echo'<td style="float:left;"><input type="checkbox" id="'.$id.'" name="'.$id.'" value="1">'.$task.'</td>';
+                                echo'<td style="float:left;"><button class="btn" name ="Done" onclick="Done()"><i class="fa fa-circle-o" aria-hidden="true"></i></button>'.$task.'</td>';
                             }
                             
                             echo'
@@ -82,14 +84,43 @@
                         
                     
                     }
-                    echo'<tr><td style="float:left; text-align:left; border-bottom: none;">Add task<td></tr>';
+                    echo'<tr id="AddTaskItem"><td style="float:left; text-align:left;"><button class="btn" name ="AddTask" onclick="Adder()"><i class="fa fa-plus-square-o" aria-hidden="true"></i> Add task</button></tr>';
                     echo'</table>';
                     
                     
                     ?>
-				
-     
+<script>
+function Adder() {
+  document.getElementById("AddTaskItem").innerHTML = '<tr><td style="float:left; text-align:left;">  <label for="task">Task:</label><input type="text" id="task" name="task"></td><td style="float:left; text-align:left;"><label for="Important">Important:</label><input type="checkbox" id="Important" name="Important" value="1"></td><td style="float:left; text-align:left;">  <label for="category">Category:</label><input type="text" id="category" name="category"></td><td style="float:left; text-align:left;"><button class="btn" name ="Save" onclick="Save()"><i class="fa fa-floppy-o" aria-hidden="true"></i> Save</button></td></tr>';
+}
+                    
+
+</script>
+               
+<script>
+    function Save(){
+<?php
+    $task = $_POST['task'];
+    if(isset($_POST['Important'])) {$imp = 1;} else{$imp = 0;}
+    $category= $_POST['category'];
+    
+   
+    
+    $sql = 'INSERT INTO tasks VALUES (NULL, :task, :category, :important, 0) ';
+    $values = [ 'task'=> $task, 'category'=>$category, 'important'=>$imp];
+    
+    $query = $db->prepare($sql);
+    $query->execute($values);
+    
+    ?>
+    
+    
+    
+}
+</script>
+    
                 </main>
+
                 </div>
                 
                 
